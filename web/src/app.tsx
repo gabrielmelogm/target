5fb7a798601592a51a8dd464ae9bcc9f211d7152
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-10-17 14:39:04 -0300

    💄 Create empty goals component

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,24 +1,11 @@
-import { Plus } from "lucide-react";
-import { Button } from "./components/ui/button";
-import { Dialog, DialogTrigger } from "./components/ui/dialog";
+import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
+import { EmptyGoals } from "./components/empty-goals";
 
 export function App() {
   return (
     <Dialog>
-      <div className="h-screen flex flex-col items-center justify-center gap-8">
-        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-          Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
-          mesmo?
-        </p>
-
-        <DialogTrigger asChild>
-          <Button type="button">
-            <Plus className="size-4" />
-            Cadastrar meta
-          </Button>
-        </DialogTrigger>
-      </div>
+      <EmptyGoals />
 
       <CreateGoal />
     </Dialog>
---
+++ web/src/components/empty-goals.tsx
@@ -0,0 +1,21 @@
+import { Plus } from "lucide-react";
+import { Button } from "./ui/button";
+import { DialogTrigger } from "./ui/dialog";
+
+export function EmptyGoals() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
+        mesmo?
+      </p>
+
+      <DialogTrigger asChild>
+        <Button type="button">
+          <Plus className="size-4" />
+          Cadastrar meta
+        </Button>
+      </DialogTrigger>
+    </div>
+  );
+}
