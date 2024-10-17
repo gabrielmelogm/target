8c330491daf18fce19084e97844f113e49ce11d2
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-10-17 13:41:48 -0300

    ✨ Input component

---
+++ web/src/components/ui/input.tsx
@@ -0,0 +1,21 @@
+import { ComponentProps, forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+type InputProps = ComponentProps<"input">;
+
+const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  return (
+    <input
+      {...props}
+      ref={ref}
+      className={twMerge(
+        "px-4 h-12 bg-black border border-zinc-900 rounded-lg placeholder-zinc-400 outline-none text-sm hover:border-zinc-800",
+        props.className,
+      )}
+    />
+  );
+});
+
+Input.displayName = "Input";
+
+export { Input };
